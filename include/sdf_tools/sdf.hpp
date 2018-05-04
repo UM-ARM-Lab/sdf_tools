f75f7d3dfd0aa4b953149e5ac46a4cdaece1f94d
Dale McConachie <dmcconac@umich.edu>
2018-05-04 13:21:01 -0400

    Added typedef.

--- include/sdf_tools/sdf.hpp
+++ include/sdf_tools/sdf.hpp
@@ -88,6 +88,8 @@ namespace sdf_tools
 
         EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
+        typedef std::shared_ptr<SignedDistanceField> Ptr;
+
         SignedDistanceField(std::string frame, double resolution, double x_size, double y_size, double z_size, float OOB_value);
 
         SignedDistanceField(const Eigen::Isometry3d& origin_transform, std::string frame, double resolution, double x_size, double y_size, double z_size, float OOB_value);
