30ef1abe18084863154c4c7b98f8065fc76e7d52
Peter Mitrano <pmitrano@umich.edu>
2019-07-01 09:45:23 -0400

    adding more useful functions

--- src/sdf_tools/utils.py
+++ src/sdf_tools/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 import sdf_tools
 
 
@@ -23,9 +24,9 @@ def compute_2d_sdf_and_gradient(grid_world, sdf_resolution, sdf_origin, frame='/
     occupied_value = sdf_tools.COLLISION_CELL(1)
 
     # 2d means this should be one cell in z
-    z_height = sdf_resolution
+    z = sdf_resolution
 
-    grid = sdf_tools.CollisionMapGrid(origin_transform, frame, sdf_resolution, x_width, y_height, z_height, oob_value)
+    grid = sdf_tools.CollisionMapGrid(origin_transform, frame, sdf_resolution, x_width, y_height, z, oob_value)
     for x_index in range(grid.GetNumXCells()):
         for y_index in range(grid.GetNumYCells()):
             occupied = (grid_world[y_index, x_index] == 1)
@@ -35,6 +36,8 @@ def compute_2d_sdf_and_gradient(grid_world, sdf_resolution, sdf_origin, frame='/
     sdf_voxelgrid = sdf_result[0]
     np_sdf = np.array(sdf_voxelgrid.GetRawData())
     np_sdf = np_sdf.reshape(sdf_voxelgrid.GetNumXCells(), sdf_voxelgrid.GetNumYCells())
+    # TODO: figure out why we must transpose SDF
+    np_sdf = np.transpose(np_sdf, [1, 0]).astype(np.float32)
 
     def gradient_function(x_index, y_index, z_index, enable_edge_gradients=False):
         return sdf_voxelgrid.GetGradient(x_index, y_index, z_index, enable_edge_gradients)
@@ -44,5 +47,39 @@ def compute_2d_sdf_and_gradient(grid_world, sdf_resolution, sdf_origin, frame='/
     np_gradient = np.array(gradient)
     d = np_gradient.shape[1]
     np_gradient = np_gradient.reshape(gradient_voxelgrid.GetNumXCells(), gradient_voxelgrid.GetNumYCells(), d)
+    # remove the z gradient
+    np_gradient = np_gradient[:, :, 0:2]
+    # TODO: figure out why we must transpose x/y gradients???
+    np_gradient = np.transpose(np_gradient, [1, 0, 2]).astype(np.float32)
+    np_gradient = np_gradient.astype(np.float32)
+    dx = np.copy(np_gradient[:, :, 0])
+    dy = np.copy(np_gradient[:, :, 1])
+    np_gradient[:, :, 0] = dy
+    np_gradient[:, :, 1] = dx
 
     return np_sdf, np_gradient
+
+
+def compute_gradient(sdf):
+    np_sdf = np.array(sdf.GetRawData())
+    np_sdf = np_sdf.reshape(sdf.GetNumXCells(), sdf.GetNumYCells())
+
+    def gradient_function(x_index, y_index, z_index, enable_edge_gradients=False):
+        return sdf.GetGradient(x_index, y_index, z_index, enable_edge_gradients)
+
+    gradient_voxelgrid = sdf.GetFullGradient(gradient_function, True)
+    gradient = gradient_voxelgrid.GetRawData()
+    np_gradient = np.array(gradient)
+    d = np_gradient.shape[1]
+    np_gradient = np_gradient.reshape(gradient_voxelgrid.GetNumXCells(), gradient_voxelgrid.GetNumYCells(), d)
+    # remove the z gradient
+    np_gradient = np_gradient[:, :, 0:2]
+
+    return np_sdf, np_gradient
+
+
+def sdf_to_np(sdf):
+    np_sdf = np.array(sdf.GetRawData())
+    np_sdf = np_sdf.reshape(sdf.GetNumXCells(), sdf.GetNumYCells())
+
+    return np_sdf
