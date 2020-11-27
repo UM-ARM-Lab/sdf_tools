better test and fix gradient

src/sdf_tools/utils_2d.py
@@ -35,8 +35,10 @@ def compute_sdf_and_gradient(grid_world, sdf_resolution, sdf_origin, frame='worl
     sdf_result = grid.ExtractSignedDistanceField(oob_value.occupancy, False, False)
     sdf_voxelgrid = sdf_result[0]
     np_sdf = np.array(sdf_voxelgrid.GetRawData())
+    # NOTE: when the data is flattened in C++, it is done so in column-major order. In 3d it's z,y,x order meaning that
+    #  in memory [x=0,y=0,z=0] is followed by [0,0,1] and [0,0,2].
+    #  However, the numpy reshape method assumes row major. Therefore we must transpose.
     np_sdf = np_sdf.reshape(sdf_voxelgrid.GetNumXCells(), sdf_voxelgrid.GetNumYCells())
-    # TODO: figure out why we must transpose SDF
     np_sdf = np.transpose(np_sdf, [1, 0]).astype(np.float32)
 
     def gradient_function(x_index, y_index, z_index, enable_edge_gradients=False):
@@ -46,16 +48,12 @@ def compute_sdf_and_gradient(grid_world, sdf_resolution, sdf_origin, frame='worl
     gradient = gradient_voxelgrid.GetRawData()
     np_gradient = np.array(gradient)
     d = np_gradient.shape[1]
-    np_gradient = np_gradient.reshape(gradient_voxelgrid.GetNumXCells(), gradient_voxelgrid.GetNumYCells(), d)
+    np_gradient = np_gradient.reshape([gradient_voxelgrid.GetNumXCells(), gradient_voxelgrid.GetNumYCells(), d])
     # remove the z gradient
     np_gradient = np_gradient[:, :, 0:2]
-    # TODO: figure out why we must transpose x/y gradients???
+    #
     np_gradient = np.transpose(np_gradient, [1, 0, 2]).astype(np.float32)
     np_gradient = np_gradient.astype(np.float32)
-    dx = np.copy(np_gradient[:, :, 0])
-    dy = np.copy(np_gradient[:, :, 1])
-    np_gradient[:, :, 0] = dy
-    np_gradient[:, :, 1] = dx
 
     return np_sdf, np_gradient
 
@@ -71,7 +69,7 @@ def compute_gradient(sdf):
     gradient = gradient_voxelgrid.GetRawData()
     np_gradient = np.array(gradient)
     d = np_gradient.shape[1]
-    np_gradient = np_gradient.reshape(gradient_voxelgrid.GetNumXCells(), gradient_voxelgrid.GetNumYCells(), d)
+    np_gradient = np_gradient.reshape([gradient_voxelgrid.GetNumXCells(), gradient_voxelgrid.GetNumYCells(), d])
     # remove the z gradient
     np_gradient = np_gradient[:, :, 0:2]
 

test/demo_bindings.py
@@ -22,9 +22,17 @@ def main():
     dt = time() - t0
     print('time: {}s'.format(dt))
 
+    plt.figure()
+    plt.title("SDF")
     plt.imshow(np.flipud(sdf))
-    plt.show()
 
+    plt.figure()
+    plt.title("Gradient")
+    xx, yy = np.meshgrid(range(x_width), range(y_height))
+    plt.quiver(xx, yy, sdf_gradient[:, :, 0], sdf_gradient[:, :, 1])
+    plt.axis("equal")
+
+    plt.show()
 
 if __name__ == '__main__':
     main()

test/test_bindings.py
@@ -11,8 +11,8 @@ class TestSDFTools(unittest.TestCase):
 
     def test_sdf_tools(self):
         res = 0.05
-        x_width = 100
-        y_height = 100
+        x_width = 20
+        y_height = 40
         grid_world = np.zeros([y_height, x_width], dtype=np.uint8)
         grid_world[1, 3] = 1
         center_x = 0
@@ -26,7 +26,11 @@ class TestSDFTools(unittest.TestCase):
         self.assertAlmostEqual(sdf[0, 3], res)
         self.assertAlmostEqual(sdf[1, 2], res)
         self.assertAlmostEqual(sdf[1, 4], res)
-        self.assertGreater(sdf[3, 6], 3*res)
+        self.assertGreater(sdf[3, 6], 3 * res)
+        self.assertEqual(sdf.shape, (y_height, x_width))
+
+        self.assertEqual(sdf_gradient.shape, (y_height, x_width, 2))
+        np.testing.assert_allclose(sdf_gradient[1, 4], [1.5, 0])
 
 
 if __name__ == '__main__':