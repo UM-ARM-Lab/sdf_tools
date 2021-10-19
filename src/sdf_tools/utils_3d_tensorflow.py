ab06b441d8f667543e1aa49a47c49928777ce691
Peter <peter@armnova>
2021-10-18 21:06:00 -0400

    fix

--- src/sdf_tools/utils_3d_tensorflow.py
+++ src/sdf_tools/utils_3d_tensorflow.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 
-from sdf_tools.utils_3d import compute_sdf_and_gradient
+from sdf_tools.utils_3d import compute_sdf_and_gradient as _compute_sdf_and_gradient
 
 
 def compute_sdf_and_gradient_batch(env, res, origin_point, batch_size: int):
     sdfs = []
     sdf_grads = []
     for b in range(batch_size):
-        sdf, sdf_grad = compute_sdf_and_gradient(env[b], res[b], origin_point[b])
+        sdf, sdf_grad = _compute_sdf_and_gradient(env[b], res[b], origin_point[b])
         sdfs.append(sdf)
         sdf_grads.append(sdf_grad)
     sdf_batch = tf.stack(sdfs, 0)
@@ -27,4 +27,4 @@ def compute_sdf_and_gradient(env, res, origin_point):
     if isinstance(origin_point, tf.Tensor):
         origin_point = origin_point.numpy()
 
-    return compute_sdf_and_gradient(env, res, origin_point)
+    return _compute_sdf_and_gradient(env, res, origin_point)
